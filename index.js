f2bbdfe8119bf6ed9baa4cbf54396d03be5b4d0e
Mitchell Gaertner <mitchell.gaertner@gmail.com>
2021-04-23 14:13:44 -0600

    Bug fixes and small updates

--- index.js
+++ index.js
@@ -4,23 +4,27 @@ const client = new Discord.Client();
 let token = process.argv[2];
 client.login(token);
 
-const prefix = ["I'm", 'Im', 'im', "i'm"];
-
+const prefix = ["I'm", 'Im', 'im', "i'm", 'I’m'];
+let commandBody;
 client.on("message", function(message){
     if (message.author.bot) return;
     let startsWith = false;
     for (var i = 0; i < prefix.length; i++){
-        startsWith = message.content.startsWith(prefix[i]);
-        if (startsWith) break;
+        if (message.content.startsWith(prefix[i])){
+            startsWith = true;
+        }
+        if (startsWith) {
+            commandBody = message.content.slice(prefix[i].length);
+            break;
+        }
     }
-    if (startsWith){
-        const commandBody = message.content.slice(prefix.length);
+    if (startsWith){         
         if (commandBody.length < 32){
-            message.member.setNickname(commandBody);        
+            message.member.setNickname(commandBody);
+            message.reply(`Hi ${commandBody}, I'm a bot`);
         }
     } else {
         return;
     }
     
 });
-
